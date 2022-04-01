3436143d577f34ccb3e8bc17392b0e35e48794c1
Bruno Lovric <brunoolovric@gmail.com>
2022-04-01 20:04:55 +0200

    Bracket notation

---
+++ ex12/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+  var lastName = "Tesla";
+
+  var thirdLetterOfLastName = lastName[2];
+
+  return thirdLetterOfLastName;
+
+}
+
+console.log(main());
+
+module.exports = main;
