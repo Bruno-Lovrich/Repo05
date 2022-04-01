4b8f2282c7975bb2f530a03cffc5abd482ca1591
Bruno Lovric <brunoolovric@gmail.com>
2022-04-01 10:30:52 +0200

    Declare string variable

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+  var myFirstName = "James";
+  var myLastName = "Brown";
+  var myHomeTown = "Barnwell";
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+  };
+
+}
+
+console.log(main());
+
+module.exports = main;
